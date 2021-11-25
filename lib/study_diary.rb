590ec2203e6521b591670d2d8328f024fbfe03fa
rotuloveio <romulor.p@hotmail.com>
2021-11-25 16:58:09 -0300

    Validação de entrada no delete_or_done;

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -143,12 +143,15 @@ def delete_or_done(done)
     print('Escolha o item [0 p/ voltar]: ')
     index = gets.to_i
   end
+
+  return if index.zero?
+
   task = filtered_itens[index - 1]
   clear
   done ? puts('MARCAR COMO FEITO'.green) : puts('EXCLUIR UM ITEM'.green)
   print("Item \"#{task.title}\" da categoria \"#{Categoria.all[category - 1]}\" ")
   done ? puts('marcado como feito.') : puts('excluído com sucesso.')
-  Tarefa.delete_or_done(task, done) unless index.zero?
+  Tarefa.delete_or_done(task, done)
 end
 
 def continue
